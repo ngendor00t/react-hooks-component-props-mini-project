ac4f367a8b7a1c35e9be575d09a591b015272d2b
Reginawambui <ngendoanita8@gmail.com>
2023-11-20 08:03:29 +0300

    done

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+export default function About(props){
+return(
+<aside>
+<img src ="https://via.placeholder.com/215"  alt = 'blog logo'/>
+<p>{props.text} </p>
+
+</aside>
+
+)
+
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import Articlelist from "./Articlelist";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <Header name = "underreacted"/>
+    <About image = "logo"
+            text ="a blog about learning react"
+/>
+    < Articlelist = ""    />
     </div>
   );
 }
