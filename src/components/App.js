a26bf696ee98d7171c26fec6be542725978ca960
Reginawambui <ngendoanita8@gmail.com>
2023-11-24 13:26:30 +0300

    putting together

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-import Articlelist from "./Articlelist";
+import ArticleList from "./Articlelist";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-    <Header name = "underreacted"/>
-    <About image = "logo"
-            text ="a blog about learning react"
-/>
-    < Articlelist = ""    />
+    <Header name = "underreacted" />
+    <About image = {image} />
+            text ="a blog about learning react" 
+
+    < ArticleList articles={blogData.blogData} />
     </div>
   );
 }
--- src/components/Articlelist.js
+++ src/components/Articlelist.js
@@ -1,10 +1,22 @@
-export default function Articlelist (){
+export default function Articlelist (props){
+
+    const{posts}=props
+
+return(
 <main>
-
-
+{articles.map((article) => (
+             <Article
+                key={article.id}
+                title={article.title}
+                date={article.date || "January 1, 1970"}
+                preview={article.preview}
+                minutes={article.minutes}
+             />
+            ))}
 
 
 
 </main>
+)
 
 }
